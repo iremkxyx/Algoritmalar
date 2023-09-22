778dfcdc4acc323bf016838d9e519289f6b2324b
İrem <irem000kaya@gmail.com>
2023-09-23 00:52:06 +0300

    algoritmalar

---
+++ Algoritmalar/SifreKirmaSezar/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SifreKirmaSezar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Şifrelenmiş kelimeyi gir:");
+            string sifreKelime = Console.ReadLine();
+            string sifrelenmisKelime = sifreKelime.ToLower();
+            string cozumlenmisKelime = "";
+            int sayac = 0;
+
+            bool durum = true;
+
+            while (durum)
+            {
+                cozumlenmisKelime = null;
+
+                for(int i = 0; i < sifrelenmisKelime.Length; i++)
+                {
+                    if (sifrelenmisKelime[i] - sayac >0 &&
+                        sifrelenmisKelime[i] - sayac <127 )
+                    {
+                        cozumlenmisKelime += Convert.ToChar(sifrelenmisKelime[i] - sayac);
+                    }
+                }
+
+                sayac++;
+                Console.WriteLine(cozumlenmisKelime);
+
+                if( sayac == 70)
+                {
+                    durum= false;
+                }
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Algoritmalar/SifrelemeSezar/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Sifreleme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Şifrelenecek kelimeyi gir:");
+            string kelime = Console.ReadLine();
+
+            Console.WriteLine("Frekans gir:");
+            int frekans = int.Parse(Console.ReadLine());
+
+            string sifrelenmisKelime = null;
+
+            for (int i = 0; i < kelime.Length; i++)
+            {
+                sifrelenmisKelime += Convert.ToChar(kelime[i] + frekans);
+
+            }
+
+
+
+            Console.WriteLine(sifrelenmisKelime);
+            Console.ReadLine();
+        }
+    }
+}
